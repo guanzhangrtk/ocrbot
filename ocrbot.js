ffacdc8b6825db914ca4f80d6067a468dfcd576c
GuanZhang <gz.lodoss@gmail.com>
2021-02-10 19:35:53 +0800

    Rebase to use discord.js; now can change language using flag emojis

--- ocrbot.js
+++ ocrbot.js
@@ -1,6 +1,6 @@
 // OCRbot version 0.1 (C)opyright 2021 GuanZhang
 
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 
 const ocrAPI = "ocr.space";
 const ocrSpaceApi = require('ocr-space-api');
@@ -29,74 +29,102 @@ require('http').createServer().listen(3000)
 
 // Send msg to the channel
 function sendDaMessage(channelID, msg, embed) {
-  bot.sendMessage({
-    to: channelID,
-    message: msg,
-    embed: embed
-  })
+  const channel = bot.channels.cache.get(channelID);
+  channel.send(msg);
 }
 
 // Parse the image attached to the message
-function parseImage(msg) {
- let url = msg.attachments[0].url;
- console.log("Processing image: %s", url);
+async function parseImage(msg) {
+ let url = msg.attachments.array()[0].url;
+
+ console.log("Processing image [%s]: %s", options.language, url);
  ocrSpaceApi.parseImageFromUrl(url, options)
    .then(function (parsedResult) {
-     sendDaMessage(reaction.d.channel_id, parsedResult.parsedText);
+     try {
+       let txt = parsedResult.parsedText;
+       if (txt == '') {
+         txt = 'Image not recognized';
+       }
+       sendDaMessage(msg.channel.id, txt);
+     } catch (e) {
+     }
      console.log('ocrParsedResult: \n', parsedResult.ocrParsedResult);
    }).catch(function (err) {
      console.log('ocrParsedResult: \n', parsedResult.ocrParsedResult);
  });
 }
 
+// Check to see attachment is an image by looking at extension
+function isImage(attachment) {
+  let imageExts = ["png", "jpg", "jpeg"]; 
+  let url = attachment.url;
+
+  for (const ext of imageExts) {
+    if (url.indexOf(ext, url.length - ext.length) !== -1) return true;
+  }
+
+  return false;
+}
+
 // Init OCRbot
 console.log("Initializing " + botname);
-var bot = new Discord.Client({
-  token: token,
-  autorun: true
-});
+// Enable partial structures for MESSAGE and REACTION to get reaction events on uncached messages
+const bot = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
+bot.login(auth.token);
 
-bot.on('ready', function (evt) {
+bot.once('ready', function (evt) {
   console.log(botname + " [" + bot.username + "] id: " + bot.id + " ready");
+
+  bot.user.setPresence({
+    activity: { name: presence }
+  })
+})
+
+// Check if message contains image
+bot.on('message', message => {
+  if (message.attachments.size <= 0) return;
+
+  if (message.attachments.every(isImage)) {
+  }
 });
 
-// Set Discord presence
-bot.setPresence({
-  game:{ name: presence }
-});
+bot.on('messageReactionAdd', async (reaction, user) => {
+  //console.log("ReactionADD: ", reaction);
+  let lang = "";
 
-bot.on('messageReactionAdd', function (reaction, userID) {
-//  console.log("ReactionADD: ", reaction);
-//  console.log(reaction + userID);
 
-  switch(reaction.d.emoji.name) {
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+    }
+  }
+
+  switch(reaction.emoji.name) {
     // Transcribe Japanese
     case '🇯🇵':
-      bot.getMessage({
-        channelID: reaction.d.channel_id,
-        messageID: reaction.d.message_id,
-        userID: reaction.d.user_id
-      }, function (err, msg) {
-         var url = msg.attachments[0].url;
-         console.log("Processing image: %s", url);
-         ocrSpaceApi.parseImageFromUrl(url, options)
-           .then(function (parsedResult) {
-             sendDaMessage(reaction.d.channel_id, parsedResult.parsedText);
-             console.log('ocrParsedResult: \n', parsedResult.ocrParsedResult);
-           }).catch(function (err) {
-             console.log('ocrParsedResult: \n', parsedResult.ocrParsedResult);
-         });
-    })
+      lang = "jpn";
     break;
 
     // Transcribe Traditional Chinese
     case '🇭🇰':
+      lang = "cht";
     break;
-
+   
     // Transcribe Simplified Chinese
     case '🇨🇳':
+      lang = "chs";
     break;
   }
+
+  if (lang != "") {
+    options.language = lang;
+    if (reaction.message.attachments.size <= 0) return;
+
+    if (reaction.message.attachments.every(isImage)) {
+      parseImage(reaction.message);
+    }
+  }  
 });
 
 // Re-connect on disconnection with 6 seconds delay
