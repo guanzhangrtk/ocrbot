896e20db54f17e54450707b5e4d502e2a47e070e
GuanZhang <44019025+guanzhangrtk@users.noreply.github.com>
2021-06-30 11:55:30 +0900

    Add support for using Google Drive API to perform OCR
    Used some Node.js sample codes from Google Drive for Developers v3 as well as https://tanaikech.github.io for reference; add new dependencies for package.json

--- ocrbot.js
+++ ocrbot.js
@@ -1,12 +1,16 @@
 // OCRbot version 0.1 (C)opyright 2021 GuanZhang
 
 var Discord = require('discord.js');
-
-const ocrAPI = "ocr.space";
 const ocrSpaceApi = require('ocr-space-api');
+const stream = require('stream');
+const request = require('request');
+const {google} = require('googleapis');
+// View and manage Google Drive files and folders created by the bot
+const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+const TOKEN_PATH = './gdrive.json';
 
 let botname = "OCRbot";
-let presence = "OCR: " + ocrAPI;
+let presence = "OCR";
 let msg = "";
 
 var token;
@@ -18,16 +22,16 @@ try {
   token = process.env.BOT_TOKEN;
 }
 
-// Load OCR options
+// Load OCRSpace options
 try {
-  var options = require("./ocr.json");
+  var options = require("./ocrspace.json");
 } catch (err) {
 }
 
 // Needed for ZEIT Now deployments
 require('http').createServer().listen(3000)
 
-// Send msg to the channel
+// Send message to the channel
 function sendDaMessage(channelID, msg, embed) {
   const channel = bot.channels.cache.get(channelID);
   channel.send(msg);
@@ -43,7 +47,7 @@ async function parseImage(msg) {
      try {
        let txt = parsedResult.parsedText;
        if (txt == '') {
-         txt = 'Image not recognized';
+         txt = parsedResult.ErrorMessage;
        }
        sendDaMessage(msg.channel.id, txt);
      } catch (e) {
@@ -64,6 +68,76 @@ function isImage(attachment) {
   }
 
   return false;
+}
+
+function streamToString (stream) {
+  const chunks = []
+  return new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk))
+    stream.on('error', reject)
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+  })
+}
+
+// Upload image to Google Drive using Drive API as a Google Doc then export Doc to plain text
+async function googleOCR(msg) {
+  const credentials= require('./google.json');
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  const token = require(TOKEN_PATH);
+  oAuth2Client.setCredentials(token);
+
+  const drive = google.drive({version: 'v3', auth: oAuth2Client});
+
+  // Upload image to Google Drive
+  let url = msg.attachments.array()[0].url;
+  console.log("Uploading image %s to GDrive", url);
+  const image = new stream.PassThrough();
+  image.on("error", (err) => console.log(err));
+  image.on("end", () => console.log("Done."));
+  await request(url).pipe(image);
+
+  const upload = await drive.files.create({
+    requestBody: {
+      name: botname + '.doc',
+      mimeType: 'application/vnd.google-apps.document'
+    },
+    media: {
+      mimeType: 'image/png',
+      body: image
+    }
+  });
+
+  // Export Google Doc to plain text (OCR)
+  try {
+    const dest = new stream.PassThrough();
+    dest.on("error", (err) => console.log(err));
+    dest.on("end", () => console.log("Done."));
+
+    const res = await drive.files.export(
+      {fileId: upload.data.id, mimeType: 'text/plain'},
+      {responseType: 'stream'}
+    );
+    await new Promise((resolve, reject) => {
+      res.data
+        .on('error', reject)
+        .pipe(dest)
+        .on('error', reject)
+        .on('finish', resolve);
+    });
+  
+    let data = await streamToString(dest);
+    // Strip first line since it always starts with a '---' divider
+    data = data.substring(data.indexOf('\n')+1);
+    sendDaMessage(msg.channel.id, data);
+
+    // Delete file after done
+    await drive.files.delete({fileId: upload.data.id});
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // Init OCRbot
@@ -92,7 +166,6 @@ bot.on('messageReactionAdd', async (reaction, user) => {
   //console.log("ReactionADD: ", reaction);
   let lang = "";
 
-
   if (reaction.partial) {
     try {
       await reaction.fetch();
@@ -115,14 +188,24 @@ bot.on('messageReactionAdd', async (reaction, user) => {
     case '🇨🇳':
       lang = "chs";
     break;
+
+    // Transcribe using Google Drive API
+    case '🇬':
+      lang = "google";
+    break;
   }
 
   if (lang != "") {
-    options.language = lang;
+    // Message has no attachment, nothing to do
     if (reaction.message.attachments.size <= 0) return;
 
     if (reaction.message.attachments.every(isImage)) {
-      parseImage(reaction.message);
+      if (lang == "google") {
+        googleOCR(reaction.message);
+      } else {
+        options.language = lang;
+        parseImage(reaction.message);
+      }
     }
   }  
 });
